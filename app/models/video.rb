2012f7058be23bd1ca9075e5239fffa8b82644e6
asavale1 <asavale1@yahoo.com>
2014-04-10 21:07:22 -0700

    video admin

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,5 @@
 class Video < ActiveRecord::Base
 	has_attached_file :clip, :styles => {
-			#:mp4 => { :geometry => "640x480", :format => 'mp4'},
 			:flv => { :geometry => "640x480", :format => 'flv'},
 			:thumb => {:geometry => "100x100#", :format => 'jpg', :time => 10},
 			:large => {:geometry => "400x400#", :format => "jpg", :time => 10}
