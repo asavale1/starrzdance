427ff5368c096ad68a50457e0db5926c64496684
asavale1 <asavale1@yahoo.com>
2014-04-11 20:17:13 -0700

    mp4 format

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,10 +1,10 @@
 class Video < ActiveRecord::Base
 	has_attached_file :clip, :styles => {
 			:flv => { :geometry => "640x480", :format => 'flv'},
-			:webm => { :geometry => "640x480", :format => 'webm'},
+			:mp4 => { :geometry => "640x480", :format => 'mp4'},
 			:thumb => {:geometry => "100x100#", :format => 'jpg', :time => 10},
 			:large => {:geometry => "400x400#", :format => "jpg", :time => 10}
-		}, :processors => [:ffmpeg]
+		}, :processors => [:ffmpeg, :qtfaststart]
 	do_not_validate_attachment_file_type(:clip)
 	validates_attachment_presence(:clip)
 end
