fcee808780426996dc1aa16df1ff8fc6ecaad96d
asavale1 <asavale1@yahoo.com>
2014-04-11 20:21:30 -0700

    remove mp4 format

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,10 +1,9 @@
 class Video < ActiveRecord::Base
 	has_attached_file :clip, :styles => {
 			:flv => { :geometry => "640x480", :format => 'flv'},
-			:mp4 => { :geometry => "640x480", :format => 'mp4'},
 			:thumb => {:geometry => "100x100#", :format => 'jpg', :time => 10},
 			:large => {:geometry => "400x400#", :format => "jpg", :time => 10}
-		}, :processors => [:ffmpeg, :qtfaststart]
+		}, :processors => [:ffmpeg]
 	do_not_validate_attachment_file_type(:clip)
 	validates_attachment_presence(:clip)
 end
