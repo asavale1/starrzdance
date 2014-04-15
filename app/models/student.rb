c7b5171f59c4fd804dac0e4fb12c1436acd92a82
asavale1 <asavale1@yahoo.com>
2014-04-14 20:42:01 -0700

    check fail

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,13 +1,13 @@
 class Student < ActiveRecord::Base
 	belongs_to :schedule
 
-	VALID_EMAIL_REGEX = /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\Z/i
+	#VALID_EMAIL_REGEX = /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\Z/i
 	LETTERS_REGEX = /\A[a-zA-Z ]+\Z/
 	NUMBERS_REGEX = /\A[1-9+]\Z/
 
 	validates :student_name, presence: true, format: {with: LETTERS_REGEX}
 	validates :parent_name, format: {with: LETTERS_REGEX}, :allow_blank => true
-	validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}
+	validates :email, presence: true
 	validates :phone, numericality: { only_integer: true}, length: { is: 10}, :allow_blank => true
 	validates :age, presence: true, numericality: { only_integer: true }
 	validates :zipcode, numericality: { only_integer: true }, length: { is: 5}, :allow_blank => true
