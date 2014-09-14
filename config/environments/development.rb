8f3c968f20da77def6fee9158337b9260924e0bd
asavale1 <asavale1@yahoo.com>
2014-09-14 15:09:01 -0700

    email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,8 +28,8 @@ Starrzdance::Application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    user_name:            'kingsavale@gmail.com',
-    password:             'Century21',
+    user_name:            ENV["GMAIL_USERNAME"],
+    password:             ENV["GMAIL_PASSWORD"],
     authentication:       'plain',
     enable_starttls_auto: true  
 
